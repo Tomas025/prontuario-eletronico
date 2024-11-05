303c439890a81fead43be9cbdb9faaf20fdb4cac
José Neto <jnan@discente.ifpe.edu.br>
2024-11-05 16:07:35 -0300

    feat: Card Nova Senha

--- src/components/CardNovaSenha/index.tsx
+++ src/components/CardNovaSenha/index.tsx
@@ -7,8 +7,9 @@ import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "../ui/form";
 import { Input } from "@/components/ui/input"
-import MailIcon from "@/utils/icons/mail.svg"
-
+import { FiMail, FiLock } from "react-icons/fi";
+import { PasswordInput } from "../ui/passwordInput";
+import { FiArrowLeft } from "react-icons/fi";
 
 const formSchema = z.object({
     username: z.string().min(2, {
@@ -26,8 +27,10 @@ export default function CardNovaSenha() {
     return (
         <Card>
             <CardHeader>
-                <Button />
-                <CardTitle>Nova Senha</CardTitle>
+                <Button className="w-5 h-10 bg-transparent focus-visible:outline-none hover:bg-transparent focus:bg-transparent active:bg-transparent border-transparent border-none shadow-none outline-none">
+                    <FiArrowLeft className="text-black" size={40}/>
+                </Button>
+                <CardTitle className="text-blue-900 text-4xl font-normal">Nova Senha</CardTitle>
             </CardHeader>
             <CardContent>
                 <Form {...form}>
@@ -39,7 +42,7 @@ export default function CardNovaSenha() {
                             <FormItem>
                                 <FormControl>
                                     <div className="flex border-b-[1px] border-gray-500">
-                                    <img src={MailIcon} alt="Ícon" className="h-5 w-5 mx-2 my-auto" />
+                                        <FiMail className="h-5 w-5 mx-2 my-auto" />
                                         <Input type="email" placeholder="Email" {...field} className="border-0 focus:outline-none rounded-none"  />
                                     </div>
                                 </FormControl>
@@ -52,7 +55,10 @@ export default function CardNovaSenha() {
                         render={({ field }) => (
                             <FormItem>
                                 <FormControl>
-                                    <Input type="text" placeholder="Código de Acesso" {...field} />
+                                    <div className="flex border-b-[1px] border-gray-500">
+                                        <FiLock className="h-5 w-5 mx-2 my-auto" />
+                                        <Input type="text" placeholder="Código de Acesso" {...field} className="border-0 focus:outline-none rounded-none"/>
+                                    </div>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -63,7 +69,10 @@ export default function CardNovaSenha() {
                         render={({ field }) => (
                             <FormItem>
                                 <FormControl>
-                                    <Input type="password" placeholder="Senha" {...field} />
+                                    <div className="flex border-b-[1px] border-gray-500">
+                                        <FiLock className="h-5 w-5 mx-2 my-auto" />
+                                        <PasswordInput type="password" placeholder="Senha" {...field} className="border-0 focus:outline-none rounded-none"/>
+                                    </div>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -74,12 +83,15 @@ export default function CardNovaSenha() {
                         render={({ field }) => (
                             <FormItem>
                                 <FormControl>
-                                    <Input type="password" placeholder="Repetir a Senha" {...field} />
+                                    <div className="flex border-b-[1px] border-gray-500">
+                                        <FiLock className="h-5 w-5 mx-2 my-auto" />
+                                        <PasswordInput type="password" placeholder="Repetir a Senha" {...field} className="border-0 focus:outline-none rounded-none"/>
+                                    </div>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
                         )}/>
-                        <Button type="submit">Submit</Button>
+                        <Button className="bg-blue-950 w-full text-lg"type="submit">Entrar</Button>
                     </form>
                 </Form>
             </CardContent>
