d25f9c848648edc6aeafd2d775343f53ecdf6e5e
Tomas025 <tomasbraz43@hotmail.com>
2024-11-19 08:53:06 -0300

    Feito card da tela de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { CardLogin } from '@/components/CardLogin';
+
+export default function Login() {
+  return (
+    <div className="min-h-screen bg-[url('/backgroundLogin.png')] bg-no-repeat bg-cover bg-center flex">
+      <CardLogin />
+    </div>
+  );
+}
