22d93596a71e215c0c560dea70f4b543a6291749
Tomas025 <tomasbraz43@hotmail.com>
2025-01-07 08:45:35 -0300

    Corrigido estilização do card de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -205,14 +205,6 @@
 
 // export default App;
 
-// export default function Home() {
-//   return <></>;
-import CardNovaSenha from '@/components/CardNovaSenha';
-
 export default function Home() {
-  return (
-    <div className="w-2/5 h-2/5">
-      <CardNovaSenha />
-    </div>
-  );
+  return <></>;
 }
