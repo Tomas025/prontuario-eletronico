6bca8678f124a030474716bf088f38dc621cf4f1
Tomas025 <tomasbraz43@hotmail.com>
2025-01-07 09:38:53 -0300

    Adicinado tela de formulario de triagem

--- src/app/(auth-routes)/triagem/page.tsx
+++ src/app/(auth-routes)/triagem/page.tsx
@@ -91,17 +91,3 @@ export default function Triagem() {
     </div>
   );
 }
-{
-  /* import { CardLogin } from '@/components/CardLogin';
-import { HeaderTriagem } from '@/components/HeaderTriagem';
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import BodyTriagem from '@/components/BodyTriagem';
-
-export default function Triagem() {
-  return (
-    <div className="flex flex-col h-screen">
-      <HeaderTriagem />
-      <BodyTriagem /> */
-}
