e3fc42af475f8bfc82ad5d2921cba216bafaa6a1
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2025-01-07 21:41:53 -0300

    Feito Schema Novo Paciente

--- src/app/(auth-routes)/atendimentos/listaAtendimentos/novoPaciente/schema/schema.ts
+++ src/app/(auth-routes)/atendimentos/listaAtendimentos/novoPaciente/schema/schema.ts
@@ -1,23 +1,61 @@
 import { z } from 'zod';
 
-export const mySchema = z
-  .object({
-    nomeCompleto: z.string({ required_error: 'Campo obrigatório' }),
-    nomeSocial: z.string().optional(),
-    dataNascimento: z
-      .string()
-      .optional()
-      .refine(
-        (data) => {
-          if (!data) return true; // Permite campo vazio
-          const dataNascimento = new Date(data);
-          return !isNaN(dataNascimento.getTime());
-        },
-        {
-          message: 'Data de nascimento inválida'
-        }
-      )
-  })
-  .required();
+export const mySchema = z.object({
+  nomeCompleto: z.string().nonempty({ message: 'Campo obrigatório' }),
+  nomeSocial: z.string().optional(),
+  dataNascimento: z
+    .string()
+    .optional()
+    .refine(
+      (data) => {
+        if (!data) return true; // Permite campo vazio
+        const dataNascimento = new Date(data);
+        return !isNaN(dataNascimento.getTime());
+      },
+      { message: 'Data de nascimento inválida' }
+    ),
+  sus: z
+    .string()
+    .optional()
+    .refine((sus) => !sus || /^\d{15}$/.test(sus), {
+      message: 'Número do SUS inválido'
+    }),
+  cpf: z
+    .string()
+    .nonempty({ message: 'Campo obrigatório' })
+    .refine((cpf) => /^\d{11}$/.test(cpf), { message: 'CPF inválido' }),
+  rg: z
+    .string()
+    .optional()
+    .refine((rg) => !rg || /^\d{9}$/.test(rg), { message: 'RG inválido' }),
+  telefone: z
+    .string()
+    .optional()
+    .refine((telefone) => !telefone || /^\d{10,11}$/.test(telefone), {
+      message: 'Telefone inválido'
+    }),
+  nomeMae: z.string().optional(),
+  cep: z
+    .string()
+    .optional()
+    .refine((cep) => !cep || /^\d{8}$/.test(cep), { message: 'CEP inválido' }),
+  cidade: z.string().optional(),
+  bairro: z.string().optional(),
+  rua: z.string().optional(),
+  numero: z
+    .string()
+    .optional()
+    .refine((numero) => !numero || /^\d+$/.test(numero), {
+      message: 'Número inválido'
+    }),
+  nomeContatoEmergencia: z.string().optional(),
+  parentesco: z.string().optional(),
+  telefoneContatoEmergencia: z
+    .string()
+    .optional()
+    .refine((telefone) => !telefone || /^\d{10,11}$/.test(telefone), {
+      message: 'Telefone de emergência inválido'
+    })
+});
 
-export type typeMyschema = z.infer<typeof mySchema>;
+export type typeMySchema = z.infer<typeof mySchema>;
